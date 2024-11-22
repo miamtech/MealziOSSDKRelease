8030913bc1d77a5e0496235b7794233fafde780a
Thomas Lemay <thomas.lemay@miam.tech>
2024-11-22 17:12:47 +0100

    update release

--- Package.swift
+++ Package.swift
@@ -3,22 +3,31 @@
 
 import PackageDescription
 
+let configurationMode = "prod"
+
 let package = Package(
-    name: "MealziOSSDKRelease",
+    name: "MealziOSSDK",
     defaultLocalization: "fr",
     platforms: [
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MealziOSSDKRelease",
-            targets: ["MealziOSSDKRelease"]),
+            name: "MealziOSSDK",
+            targets: ["MealziOSSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/miamtech/MealzCoreRelease", from: "5.2.1"),
     ],
     targets: [
         .binaryTarget(
-            name: "MealziOSSDKRelease",
-            path: "./MealziOSSDK.xcframework"
-        )
+            name: "MealziOSSDK",
+            url: "https://github.com/miamtech/MealziOSSDKRelease/raw/release/5.2.1/MealziOSSDK.zip",
+            checksum: "bf66f8e9057c6ac54efb2ec74cba46a620b3dd36f0cc572fde8eb96a7fc9ecaf"
+        ),
     ]
 )
+/*
+
+ */
